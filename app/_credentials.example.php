ca365d12eb238f3cfe31c36f2122723cf7143ff2
Murtaza Raja <murtraja@gmail.com>
2018-11-19 22:47:12 +0530

    Post request working, id is verified

---
+++ app/_credentials.example.php
@@ -0,0 +1,5 @@
+<?php
+
+$CLIENT_ID='google web type client id, the same one which is there in the app'
+
+?>
--- app/index.php
+++ app/index.php
@@ -1,12 +1,31 @@
 <?php 
-//require_once '../external/google-api-php-client/vendor/autoload.php';
+require_once '../external/google-api-php-client/vendor/autoload.php';
 //print_r($_SERVER);
 if ($_SERVER['REQUEST_METHOD'] === 'GET') {
     echo "hi";
 } 
 // else ($_SERVER['REQUEST_METHOD'] === 'POST') {
 else {
-    $data = array("token" => "34234lakskqr432l");
-    echo(json_encode($data));
+    //$request = json_decode($_REQUEST[0]);
+    // $a = array("34", "23", "234");
+    // var_dump($a);
+    // var_dump($_REQUEST);
+    // var_dump($_REQUEST[0]);
+    // $idToken = $request['idToken'];
+    // $data = array("token" => $idToken);
+    // echo(json_encode($data));
+    $post = file_get_contents('php://input');
+    $post = json_decode($post, true);
+    $idToken = $post['idToken'];
+    $CLIENT_ID = '1078083950140-jhaqi6u7n3avctus3ao5vtv5oo58nmla.apps.googleusercontent.com';
+    $client = new Google_Client(['client_id'=> $CLIENT_ID]);
+    $payload = $client->verifyIdToken($idToken);
+    if($payload) {
+        $userid = $payload['sub'];
+        echo "SUCCESS";
+    } else {
+        echo "FAIL";
+    }
+    // echo(json_encode($_REQUEST));
 }
 ?>
